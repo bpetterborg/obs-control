f8ac2631bc323a88932d03a0479db9e1bd99ed4c
Ben Petterborg <ben.petterborg@gmail.com>
2021-05-21 12:08:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,26 @@
 # TODO:
 #
 #
-#
-#
-#
 
 # imports
-import requests
+# libraries
+import requests		# communicate w/ esp32 remotes
 
+import gi # 
+gi.require_version('Gtk','3.0') # set proper version?
+from gi.repository import Gtk   # ui
+
+import obspython as obs         # communicate w/ obs
+# files where other stuff is
+import macro_board				# use a arduino-based macro board
+
+
+# variables
+VERSION = '0.0.1 alpha'
+
+
+
+# main things
+
+# gtk gui
+window = Gtk.Window(title='OBS Control v '+ version) 
