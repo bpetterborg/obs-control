00db0e27c2479948c5968266d2c75634a62ef626
Ben Petterborg <ben.petterborg@gmail.com>
2021-05-21 13:26:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,20 +28,4 @@ serial_port = 'COM5'
 
 # main stuff
 
-# gtk ui
-class MainWindow('Gtk', '3.0')
-	def __init__(self):
-		Gtk.Window.__init__(self, title='OBS Controller'
-
-		self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
-
-    def on_button_clicked(self, widget):
-        print("Button Clicked")
-
-win = MainWindow()
-win.connect('destroy', Gtk.main_quit)
-win.show_all()
-Gtk.main()
 
