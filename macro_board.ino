28eaca02adde7c6d903c813fa0295088ce7dbe6c
Ben Petterborg <ben.petterborg@gmail.com>
2021-05-26 09:14:08 -0400

    (Incomplete) add stuff for serial and pin control
    Still need complete the thing that reports pin states and make a loop to initialise pins.

--- macro_board.ino
+++ macro_board.ino
@@ -1,1 +1,39 @@
+/*
+ *	This runs on the arduino running
+ *	the macroboard. 
+ *
+ */
 
+typedef struct {
+    uint8_t pinNum;
+    bool pinVal;
+} pinInit_t
+
+// variables
+char incomingData; // might need to fix this later
+int digitalInputPins[] =
+{
+	2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+};
+int digitalOutputPins[] =
+{
+	12, 13, 14, 15, 16, 17	
+};
+
+
+// run once at start
+void setup()
+{
+	Serial.begin(9600); // start serial
+	Serial.write('Connected to Macroboard');
+	
+	// setup pins (digital inputs)
+		
+	
+	
+}
+	
+void main()
+{
+	
+}
