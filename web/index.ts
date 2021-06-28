2958003e9790ff762fd80f974e79551bed8110f6
Ben P <ben.petterborg@gmail.com>
2021-06-28 09:37:11 -0400

    Deleted unused deps

--- web/index.ts
+++ web/index.ts
@@ -10,12 +10,10 @@
 
 // imports
 const OBSWebSocket = require('obs-websocket-js');
-const express = require('express');
 var fs = require('fs');
 
 // variables
 const obs = new OBSWebSocket();
-const app = express();
 const port = 3000; // may need to change this
 
 var obs_url = 'localhost:4444';
