c51a9034d620cda8e58f6c71cbd2c9ed6461476c
Ben P <ben.petterborg@gmail.com>
2021-06-29 09:04:38 -0400

    Added stuff for web interface

--- web/index.ts
+++ web/index.ts
@@ -11,40 +11,52 @@
 // imports
 const OBSWebSocket = require('obs-websocket-js');
 var fs = require('fs');
+var http = require('http');
+
 
 // variables
 const obs = new OBSWebSocket();
 const port = 3000; // may need to change this
 
-var obs_url = 'localhost:4444';
-var obs_password = fs.readFileSync('obs_password', 'utf8');
+// for web interface
+const webserver = http.createServer((req, res) => {
+	res.writeHead(200, { 'content-type': 'text/html' });
+	fs.createReadStream('index.html').pipe(res);	
+});
+
+var obs_url = 'localhost:4444'; // have this set in webapp
+var obs_password = fs.readFileSync('obs_password', 'utf8'); // also have the set in webapp
+
+// start web interface
+webserver.listen(process.env.PORT || 3000)
 
 // connect to obs server
-obs.connect({
-        address: obs_url,
-        password: obs_password
-    })
-    
-    .then(() => {
-        console.log(`OBSWS: Successfully connected and authenticated`);
+function connectToServer() {
+	
+	obs.connect({
+			address: obs_url,
+			password: obs_password
+		})
+		
+		.then(() => {
+			console.log(`OBSWS: Successfully connected and authenticated`);
 
-        return obs.send('GetSceneList');
-    })
-    
-    .catch(err => { // Promise convention dicates you have a catch on every chain.
-        console.log(err);
-    });
+			return obs.send('GetSceneList');
+		})
+		
+		.catch(err => { // Promise convention dicates you have a catch on every chain.
+			console.log(err);
+		});
+}
 
-
+// notify user about scene changes
 obs.on('SwitchScenes', data => {
-    console.log(`OBSWS: New Active Scene: ${data.sceneName}`);
+	console.log(`OBSWS: New Active Scene: ${data.sceneName}`);
 });
 
 // for handling uncaught exceptions
 obs.on('error', err => {
-    console.error('OBSWS: socket error:', err);
+	console.error('OBSWS: socket error:', err);
 });
 
-
-
-
+connectToServer();
